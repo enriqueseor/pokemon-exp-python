214488ca4bdb91c352c06e292a52dda9f221f858
enriqueseor <enriqueseor@gmail.com>
2023-07-02 01:10:41 +0100

    pokemon exp calc english translation

--- ExpPokemon.py
+++ ExpPokemon.py
@@ -1,79 +1,80 @@
-# Calculadora Sistema Experiencia Pokemon
-#https://bulbapedia.bulbagarden.net/wiki/Experience
+# Pokémon Experience growing type
+# https://bulbapedia.bulbagarden.net/wiki/Experience
 
-def input_check(nivel):
+def input_check(level):
     valid = False
-    inrange = False
     while not valid:
         try:
-            nivel = int(input('Nivel Pokemon: '))
+            level = int(input('Pokemon level: '))
             valid = True
         except ValueError:
-            print('Esto no es un nivel')
+            print('This is not a level')
         else:
-            if nivel in range(2, 100):
+            if level in range(2, 101):
                 break
+            elif level == 0:
+                print("FINISHED")
+                exit()
             else:
-                print('Nivel fuera de Rango')
-    return nivel
+                print('level out of range')
+                break
+    return level
 
 
-def rapido(nivel):
-    e = 0.8 * nivel ** 3
-    print("rapido    ", int(e))
-
-
-def normal(nivel):
-    e = nivel ** 3
-    print("normal    ", int(e))
-
-
-def lento(nivel):
-    e = 1.25 * nivel ** 3
-    print("lento     ", int(e))
-
-
-def parabolico(nivel):
-    e = 1.2 * nivel ** 3 - 15 * nivel ** 2 + 100 * nivel - 140
-    print("parabolico", int(e))
-
-
-def erratico(nivel):
-    if 0 < nivel <= 50:
-        e = (nivel ** 3 * (100 - nivel)) / 50
-    elif 51 <= nivel <= 68:
-        e = (nivel ** 3 * (150 - nivel)) / 100
-    elif 69 <= nivel <= 98:
-        e = (nivel ** 3 * ((1911 - 10 * nivel) / 3)) / 50
+def erratic(level):
+    if 0 < level <= 50:
+        e = (level ** 3 * (100 - level)) / 50
+    elif 51 <= level <= 68:
+        e = (level ** 3 * (150 - level)) / 100
+    elif 69 <= level <= 98:
+        e = (level ** 3 * ((1911 - 10 * level) / 3)) / 50
     else:
-        e = (nivel ** 3 * (160 - nivel)) / 100
-    print("erratico  ", int(e))
+        e = (level ** 3 * (160 - level)) / 100
+    print("erratic     ", int(e))
 
 
-def fluctuante(nivel):
-    if 0 < nivel <= 50:
-        e = (nivel ** 3 * (24 + (nivel + 1) / 3)) / 50
-    elif 51 <= nivel <= 68:
-        e = (nivel ** 3 * (14 + nivel)) / 50
+def fast(level):
+    e = 0.8 * level ** 3
+    print("fast        ", int(e))
+
+
+def medium_fast(level):
+    e = level ** 3
+    print("medium fast ", int(e))
+
+
+def medium_slow(level):
+    e = 1.2 * level ** 3 - 15 * level ** 2 + 100 * level - 140
+    print("medium slow ", int(e))
+
+
+def slow(level):
+    e = 1.25 * level ** 3
+    print("slow        ", int(e))
+
+
+def fluctuanting(level):
+    if 0 < level <= 50:
+        e = (level ** 3 * (24 + (level + 1) / 3)) / 50
+    elif 51 <= level <= 68:
+        e = (level ** 3 * (14 + level)) / 50
     else:
-        e = (nivel ** 3 * (32 + (nivel / 2)) / 50)
-    print("fluctuante", int(e))
+        e = (level ** 3 * (32 + (level / 2)) / 50)
+    print("fluctuanting", int(e))
 
 
-def main(nivel=None):
+def main(level=None):
     print("press 0 to exit")
-    nivel = input_check(nivel)
-    while nivel != 0:
-        rapido(nivel)
-        normal(nivel)
-        lento(nivel)
-        parabolico(nivel)
-        erratico(nivel)
-        fluctuante(nivel)
-        nivel = input_check(nivel)
-    print("FIN")
+    level = input_check(level)
+    while level != 0:
+        fast(level)
+        medium_fast(level)
+        slow(level)
+        medium_slow(level)
+        erratic(level)
+        fluctuanting(level)
+        level = input_check(level)
 
-'''
+
 if __name__ == "__main__":
     main()
-'''
